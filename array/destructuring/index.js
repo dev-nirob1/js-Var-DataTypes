74fc7f08aa7e510f2fc99b1c910151a526069e63
dev-nirob1 <dev.nirob1@gmail.com>
2026-01-01 22:18:15 +0600

    destructuring

---
+++ array/destructuring/index.js
@@ -0,0 +1,7 @@
+const salad = ['tomato', 'corn', 'cucumber', 'carrot'];
+// const tomato = salad[0]
+// const corn = salad[1]
+// const cucumber = salad[2]
+// const carrot = salad[3]
+const [tomato, corn, cucumber, carrot] = salad;
+console.log(tomato, corn, cucumber, carrot);
