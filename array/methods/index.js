db6e54e10dd3f46be556a2f203009e62613e1f01
dev-nirob1 <dev.nirob1@gmail.com>
2026-01-02 20:32:16 +0600

    concat method

---
+++ array/methods/index.js
@@ -0,0 +1,10 @@
+// concat() method 
+const first = [1,2,3];
+const second = [4,5,6];
+const marged = first.concat(second)
+console.log(marged);
+const third = [7,8,8]
+
+// marge multiple array 
+const margedMulti = marged.concat(first, second, third)
+console.log(margedMulti);
