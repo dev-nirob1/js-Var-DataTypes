99ae326bdcdb796b42196de23c918806820abd5e
dev-nirob1 <dev.nirob1@gmail.com>
2026-01-02 22:14:32 +0600

    join method

--- array/methods/index.js
+++ array/methods/index.js
@@ -2,9 +2,22 @@
 const first = [1,2,3];
 const second = [4,5,6];
 const marged = first.concat(second)
-console.log(marged);
+// console.log(marged);
 const third = [7,8,8]
 
 // marge multiple array 
 const margedMulti = marged.concat(first, second, third)
-console.log(margedMulti);
+// console.log(margedMulti);
+
+// join() method 
+
+const num = [1,2,3]
+const result = num.join() //join element by comma(,)
+const separator = num.join('-') //separte element by (-)
+const arr = ['hello'] // empty array ba ekta element thakle sekkhetre separator kaj korbe na 
+const empArr = arr.join('-')
+// console.log(empArr);
+
+// fill() method 
+
+
