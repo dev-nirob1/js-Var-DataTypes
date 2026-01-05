9f2e6d3a577f5dbe6073a984c66e012117f44588
dev-nirob1 <dev.nirob1@gmail.com>
2026-01-05 22:36:36 +0600

    update

---
+++ array/staticMethods/index.js
@@ -0,0 +1,7 @@
+// Array static methods
+// Array.of() Methods 
+const arrOf = [2,3, true, {'item': 1}];
+const pussed = arrOf.push(2)
+console.log(arrOf);
+console.log(pussed);
+console.log(arrOf.at(3));
