4e5c993e16c95f82dc8eec5d58152eab1f505a25
dev-nirob1 <dev.nirob1@gmail.com>
2026-01-05 23:05:01 +0600

    array like

--- array/staticMethods/index.js
+++ array/staticMethods/index.js
@@ -2,6 +2,11 @@
 // Array.of() Methods 
 const arrOf = [2,3, true, {'item': 1}];
 const pussed = arrOf.push(2)
-console.log(arrOf);
-console.log(pussed);
-console.log(arrOf.at(3));
+// console.log(arrOf);
+// console.log(pussed);
+// console.log(arrOf.at(3));
+
+
+// array like 
+const li = document.getElementsByTagName('li')
+console.log(li[3]);
