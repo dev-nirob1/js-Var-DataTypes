511cc700aa51da53643b4414b0619db77a53dcf4
dev-nirob1 <dev.nirob1@gmail.com>
2026-01-20 20:24:40 +0600

    map method

--- array/iterableMethods/index.js
+++ array/iterableMethods/index.js
@@ -1,6 +1,6 @@
-    // iterable methods 
-    
-    // filter() method
+// iterable methods
+
+// filter() method
 const developers = [
   {
     id: 1,
@@ -8,7 +8,7 @@ const developers = [
     age: 22,
     role: "frontend",
     salary: 35000,
-    isActive: true
+    isActive: true,
   },
   {
     id: 2,
@@ -16,7 +16,7 @@ const developers = [
     age: 28,
     role: "backend",
     salary: 45000,
-    isActive: false
+    isActive: false,
   },
   {
     id: 3,
@@ -24,7 +24,7 @@ const developers = [
     age: 24,
     role: "fullstack",
     salary: 55000,
-    isActive: true
+    isActive: true,
   },
   {
     id: 4,
@@ -32,25 +32,64 @@ const developers = [
     age: 21,
     role: "frontend",
     salary: 30000,
-    isActive: true
-  }
+    isActive: true,
+  },
 ];
 
 const adult = developers.filter((elem) => {
-    // filter users array er sob gulo element check korbe. then condition e jegulo true asbe segulo adult array te add hobe.
-    return elem.age >=18;
-})
-console.log(adult);
-const notAdult = developers.filter(dev => {
+  // filter users array er sob gulo element check korbe. then condition e jegulo true asbe segulo adult array te add hobe.
+  return elem.age >= 18;
+});
+// console.log(adult);
+const notAdult = developers.filter((dev) => {
   return dev.age < 18;
+});
+// console.log(notAdult);
+
+// map() method
+
+// const developers = [
+//   {
+//     id: 1,
+//     name: "Rahim",
+//     age: 22,
+//     role: "frontend",
+//     salary: 35000,
+//     isActive: true
+//   },
+//   {
+//     id: 2,
+//     name: "Karim",
+//     age: 28,
+//     role: "backend",
+//     salary: 45000,
+//     isActive: false
+//   },
+//   {
+//     id: 3,
+//     name: "Hasan",
+//     age: 24,
+//     role: "fullstack",
+//     salary: 55000,
+//     isActive: true
+//   },
+//   {
+//     id: 4,
+//     name: "Nabila",
+//     age: 21,
+//     role: "frontend",
+//     salary: 30000,
+//     isActive: true
+//   }
+// ];
+
+const frontendDeveloper = developers.map((data) => {
+  return data.role == "frontend" && data.name; //returned map() array will have same length as original array;
+  //array transform korte map use kora hoy 
+});
+// console.log(frontendDeveloper);
+const devNameWithRole = developers.map(dev => {
+    dev['devNameRole'] = dev.name + dev.role;
+    return dev //added a new property to every element.
 })
-console.log(notAdult);
-
-
-// map() method 
-
-const users = [
-  { name: 'Rahim', age: 17 },
-  { name: 'Karim', age: 22 },
-  { name: 'Hasan', age: 19 }
-];
+console.log(devNameWithRole);
